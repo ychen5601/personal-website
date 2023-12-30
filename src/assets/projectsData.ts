7d8a4e96532c75f9a37b8ef6ebc3b81b88363397
Yu Yang Chen <yuyangchen@Yus-MacBook-Pro.local>
2023-12-29 19:58:33 -0800

    Langchain longdesc

--- src/assets/projectsData.ts
+++ src/assets/projectsData.ts
@@ -11,12 +11,18 @@ const projects = [
 
       Another goal of mine is MongoDB integration, allowing visitors to comment below my projects and leave constructive feedback on anything related to my work, enabling me to become a better developer. This is currently a work in progress.
 
-      This project was heavily inspired stylistically by my friend Davis' portfolio website, which I will link below as well.`},
+      This project was heavily inspired stylistically by my friend Davis' portfolio website, which I will link below as well.`
+    },
     {title: "LangChain JS", 
       shortDesc: "Contribution to open-source LLM management libraries in TypeScript.", 
       imageUrl: require("../assets/langchain_logo.png"), 
       githubUrl: "https://github.com/langchain-ai/langchainjs/pull/3211", 
-      longDesc: "placeholder"},
+      longDesc: `With Large Language Models (LLMs) being all the craze currently, paired with a newfound motivation to seek and contribute to open-source projects, my team and I decided to explore the intricacies of AI, as well as its uses, by contributing to the LangChain repository.
+      
+      LangChain is an open-source library designed to streamline the development and deployment of LLM applications. It provides tools and frameworks to facilitate the integration of LLMs like GPT into software systems, allowing developers and users to harness the power of AI in their applications. 
+      
+      My team and I scoped, implemented, and tested the Conversation Token Memory module, which facilitates precise control over memory usage when communicating with the LLM, by defining memory usage based on the number of allocated tokens.`
+    },
     {title: "Fitbook", 
       shortDesc: "Fitness social media mobile app using React Native and MongoDB in TypeScript.", 
       imageUrl: require("../assets/fitbook_logo.png"), 
