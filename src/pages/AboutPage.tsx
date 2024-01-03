28473901552f37e0ed14bc362af6a2ad8bc82243
Yu Yang Chen <yuyangchen@Yus-MacBook-Pro.local>
2024-01-02 17:18:54 -0800

    Add coding photo, paragraph for team player page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,6 +1,8 @@
 import ReactPageScroller from 'react-page-scroller';
 import about from '../assets/about';
 import React from 'react';
+import codePhoto from '../assets/code-photo.jpeg';
+import teamPlayer from '../assets/teamPlayer';
 
 function AboutPage() {
 
@@ -14,33 +16,62 @@ function AboutPage() {
     };
 
     const contentStyle = {
-     
+        
         alignItems: "center",
         paddingTop: "19%",
         marginLeft: 8,
     };
 
+    const bodyAndImgContainer = {
+        display: "flex"
+    }
+
     const bodyStyle = {
+        display: "flex",
         marginRight: 8,
         marginTop: 8,
         color: "#415B41",
     }
+
+    const imageStyles = {
+        borderRadius: 70,
+        height: 140,
+        width: 140,
+        marginRight: 8,
+        marginTop: 24,
+      }
 
     return (
         <ReactPageScroller>
             <div style={pageStyle}>
                 <div style={contentStyle}>
                     <header>{about.title}</header>
-                    <body style={bodyStyle}>
-                        {about.text.split('\n').map((line: string, index: number) => (
-                        <React.Fragment key={index}>
-                            {line} <br />
-                        </React.Fragment>
-                        ))}
-                    </body>
+                    <div style={bodyAndImgContainer}>
+                        <body style={bodyStyle}>
+                            {about.text.split('\n').map((line: string, index: number) => (
+                            <React.Fragment key={index}>
+                                {line} <br />
+                            </React.Fragment>
+                            ))}
+                        </body>
+                        <img src={codePhoto} style={imageStyles}/>
+                    </div>
                 </div>
             </div>
-            <div style={pageStyle}>page 2</div>
+            <div style={pageStyle}>
+                <div style={contentStyle}>
+                    <header>{teamPlayer.title}</header>
+                    <div style={bodyAndImgContainer}>
+                        <body style={bodyStyle}>
+                            {teamPlayer.text.split('\n').map((line: string, index: number) => (
+                            <React.Fragment key={index}>
+                                {line} <br />
+                            </React.Fragment>
+                            ))}
+                        </body>
+                    </div>
+                </div>
+            </div>
             <div style={pageStyle}>page 3</div>
             <div style={pageStyle}>page 4</div>
         </ReactPageScroller>
