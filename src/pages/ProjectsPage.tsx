1f0ed894acb6fc24bf962b9e189f91d7437c0328
Yu Yang Chen <kennyyuyang.chen@mail.utoronto.ca>
2024-05-16 22:18:53 -0400

    feat: add mobile support for projects page

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,25 +1,38 @@
 import ProjectCard from "../components/ProjectCard";
 import projects from "../assets/projectsData";
 import { CardActionArea } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProjectDialog from "../components/ProjectDialog";
 
 function ProjectsPage() {
 
   const [openModalId, setOpenModalId] = useState<number | null>(null);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const handleOpen = (index: number) => {
     setOpenModalId(index);
-  }
+  };
 
   const handleClose = () => {
     setOpenModalId(null);
-  }
+  };
 
   const viewStyle = {
     display: "flex",
     justifyContent: "center",
-  }
+  };
 
   const cardContainer = {
     display: "flex",
@@ -30,7 +43,18 @@ function ProjectsPage() {
     marginLeft: 10,
     marginRight: 10,
     flexWrap: "wrap" as "wrap",
-  }
+  };
+
+  const cardContainerMobile = {
+    display: "flex",
+    gap: 40,
+    paddingTop: 20,
+    paddingBottom: 20,
+    width: 280,
+    marginLeft: 10,
+    marginRight: 10,
+    flexWrap: "wrap" as "wrap",
+  };
 
   const cardActionStyle = {
     fontFamily: "poppins",
@@ -39,11 +63,18 @@ function ProjectsPage() {
     backgroundColor: "#6B5B51",
     borderRadius: 15,
     transition: "all 0.3s ease",
-};
+  };
+
+  function getCardContainer() {
+    if (width > 768) {
+      return cardContainer;
+    }
+    return cardContainerMobile;
+  }
 
   return (
     <div style={viewStyle}>
-      <div style={cardContainer}>
+      <div style={getCardContainer()}>
         {projects.map((project, index) => (
           <div>
           <CardActionArea style={cardActionStyle} onClick={() => handleOpen(index)} disableRipple>
